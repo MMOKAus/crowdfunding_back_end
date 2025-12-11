38a86b18f4f75a7e6f8f65ee340137a114e41950
Olga Kozlovskaia <sapienzaok@gmail.com>
2025-12-11 13:55:28 +1000

    Add PawFund banner

--- crowdfunding/crowdfunding/urls.py
+++ crowdfunding/crowdfunding/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from users.views import CustomAuthToken
+from django.http import JsonResponse
+
+def api_root(request):
+    return JsonResponse({"message": "Welcome to the PawFund API"})
 
 urlpatterns = [
     path('admin/', admin.site.urls),
